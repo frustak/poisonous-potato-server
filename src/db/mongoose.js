be0b05f5e16f2bb58606b3328b8339aeffb72a55
Frostack <soulburn007@gmail.com>
2020-06-29 14:54:21 +0430

    🗂 add public database url

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/tupik-api', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  'mongodb+srv://frostack:qNsw9YbkYxwsABDg@cluster0-kbmnh.azure.mongodb.net/pp-api?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true,
+  }
+);
